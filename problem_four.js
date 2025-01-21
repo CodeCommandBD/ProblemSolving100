261f8666f5ae9744d5c338b7912e047cca49ca12
CodeCommandBD <shantokumar22@gmail.com>
2025-01-21 11:24:02 +0600

    ThreeSolve

---
+++ problem_four.js
@@ -0,0 +1,11 @@
+const secuence = (inputArray) =>{
+    for(let i=1; i<inputArray.length; i++){
+        if(inputArray[i] < inputArray [i -1] ){
+            return false
+        }
+    }
+    return true
+}
+
+console.log(secuence([1,2,3]));
+console.log(secuence([1,10,3]));

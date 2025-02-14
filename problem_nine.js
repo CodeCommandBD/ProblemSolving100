acca5816c247ba0e1928149c9b2d8edd8d2350a2
CodeCommandBD <shantokumar22@gmail.com>
2025-02-14 21:47:27 +0600

    NineSolve

---
+++ problem_nine.js
@@ -0,0 +1,9 @@
+//create a function to return the amount of potatoes there are in a string
+
+
+const potato = (str)=>{
+    let count = str.split('potato');
+    console.log(count.length-1);
+    
+}
+potato('potatoes')

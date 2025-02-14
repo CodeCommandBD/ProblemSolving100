a6512d0ce4487d36cc33f8abefe47b358ee23784
CodeCommandBD <shantokumar22@gmail.com>
2025-02-14 21:59:30 +0600

    TenSolve

---
+++ problem_ten.js
@@ -0,0 +1,21 @@
+//two take input a function 
+
+
+// a+b =10 a= 10 b=10 
+
+// condition er opre output = true or false 
+
+
+// const checkTen = (a,b) =>{
+//     console.log([a,b,a+b].includes(10));
+    
+// }
+
+// other way 
+
+const checkTen = (a,b) =>{
+  return  a==10 || b == 10 || a+b ==10
+    
+}
+
+console.log(checkTen(5,1));
